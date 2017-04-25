0f4c493d389db4e9c4920825153abdf4fe19a01a
Le0nX <cloudmindone@gmail.com>
2017-04-25 06:41:48 +0300

    some chgs

--- test_script.py
+++ test_script.py
@@ -1,8 +1,17 @@
 import re
+import numpy as np
 
 with open('sentences.txt') as text:
 	content = text.read()
+content = content.lower()
+#content.replace("'", "")
+
 words = re.findall(r'[a-z]+', content)
 
+word_dict = {}
+for word in words:
+	if word not in word_dict:
+		word_dict[word] = len(word_dict)
 
-print(words)
+d = max(i for i in word_dict.values())
+print d
