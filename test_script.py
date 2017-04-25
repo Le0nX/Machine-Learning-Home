9ee48dba373ec20b4dfc308b51ad9f0b5a061968
Le0nX <cloudmindone@gmail.com>
2017-04-25 07:55:15 +0300

    cool re

--- test_script.py
+++ test_script.py
@@ -12,6 +12,16 @@ word_dict = {}
 for word in words:
 	if word not in word_dict:
 		word_dict[word] = len(word_dict)
-
 d = max(i for i in word_dict.values())
-print d
+
+with open('sentences.txt') as text:
+	sentences = text.readlines()
+
+i = 0
+for sen in sentences:
+	sentences[i] = sentences[i].lower()
+	sentences[i] = re.sub(r"['-?,.!\n]", '', sentences[i])
+	i += 1
+print sentences
+#for value in word_dict.values():
+#	i = 0
